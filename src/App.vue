6f4c358f8fd65268b2331c3a80347f019e16a3c0
Panpan <2398577209@qq.com>
2026-04-16 14:41:47 +0800

    大删减

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
-  <router-view></router-view>
+  <router-view />
 </template>
 
+<script setup>
+</script>
+
 <style>
-/* 1. 终极清除白边魔法 */
-*, *::before, *::after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-/* 2. 强制 html 和 body 铺满全屏，且隐藏多余的页面滚动条 */
 html, body, #app {
-  width: 100%;
-  height: 100%;
   margin: 0;
   padding: 0;
-  overflow: hidden; /* 砍掉外层滚动条 */
-}
-
-/* 3. 字体优化，让文字更清晰 */
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  height: 100%;
+  width: 100%;
+  background-color: #f5f7fa;
 }
 </style>
